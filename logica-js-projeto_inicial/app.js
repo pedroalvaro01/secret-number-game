bee6bf7954bfdc7a936c574286de7443dde44d5a
Pedro Álvaro <pedro.alvaro0112@gmail.com>
2024-10-22 15:13:05 -0300

    add update while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,20 @@
 alert('Seja bem-vindo ao jogo do número secreto');
-let numeroSecreto = 7;
+let numeroSecreto = 3;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute
 
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Poxa... Infelizmente, você errou. :(')
+// enquanto o chute não for igual ao n.s
+while (chute != numeroSecreto) {
+   chute = prompt('Escolha um número entre 1 e 30');
+   // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você descobriu o número secreto é ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    } 
 }
+
