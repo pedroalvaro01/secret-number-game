e19aa667453c5d4d05a21e98775c204a147225ab
Pedro Álvaro <pedro.alvaro0112@gmail.com>
2024-10-22 11:44:37 -0300

    add alert message

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,10 @@
 alert('Seja bem-vindo ao jogo do número secreto');
-let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+    alert('Parabéns! Você descobriu o número secreto (5)');
+} else {
+    alert('Poxa... Infelizmente, você errou. :(')
+}
