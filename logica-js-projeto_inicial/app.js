90e36217680774e61a7fee92b1f575a2c5eb9a36
Pedro Álvaro <pedro.alvaro0112@gmail.com>
2024-10-22 11:57:02 -0300

    add variables

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,10 @@
 alert('Seja bem-vindo ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 7;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if (chute == numeroSecreto) {
-    alert('Parabéns! Você descobriu o número secreto (5)');
+    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
     alert('Poxa... Infelizmente, você errou. :(')
 }
